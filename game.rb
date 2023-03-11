80a305c1e7dadb490e6ffee62b346b4837885266
Jason Crockett <JesusIsLord33333@outlook.com>
2023-03-11 07:29:21 +0000

    get winning bets is done, now to collate them.

--- game.rb
+++ game.rb
@@ -1,8 +1,32 @@
+require_relative 'lineup'
 class Game
+  attr_reader :winning_each_way_bets
+  attr_reader :game_name
   def initialize(game_hash)
     @game = game_hash
     @bet_ids = get_unique_bet_ids
     @winning_each_way_bets = get_winning_bets
+    puts @game["fixture"]
+    @lineup = Lineup.new(@game["fixture"]["id"])
+    @lineup.get
+    begin
+      @game_name = "#{@lineup.home_team} vs #{@lineup.away_team}"
+    rescue
+      @game_name = "unknown"
+    end
+  end
+  def sort_bets_by_odds_by_type
+    ids = @winning_each_way_bets.map do |booky|
+      booky["odds"].each do |bet|
+        bet["id"]
+      end
+    end
+    ids.uniq!
+    ids.each do |id|
+      @winning_each_way_bets.each do |bet|
+        bet.select{|odds,v| odds["id"].to_i == id }
+      end
+    end
   end
   private
   def get_unique_bet_ids
@@ -16,15 +40,7 @@ class Game
   end
   def get_winning_bets
     @game["bookmakers"].map do |booky|
-      over_odds = booky["bets"].select do |bet|
-        bet["values"].any? do |value|
-          value["odd"].to_f >= bet["values"].count
-        end
-      end
-    #   TODO - collate over odds by bet id
-    # TODO - Then for each bet id, make sure that their are less outcomes than collated bets
-    # TODO - Then Max the odds for duplicate outcome keys
+      {booky: booky["name"],odds: booky["bets"].select(){|bet| bet["values"].any?(){|value| value["odd"].to_f >= bet["values"].count - 1}}}
     end
-
   end
 end
