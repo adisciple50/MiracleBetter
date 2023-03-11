737daf4a8c6cf308e42f5852785defaae4941e07
Jason Crockett <JesusIsLord33333@outlook.com>
2023-03-11 07:29:50 +0000

    get winning bets is done, now to collate them.

--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 require_relative 'football_odds'
+require_relative 'game_factory'
 odds = FootballOdds.new
 begin
   odds.load_odds_from_file_if_current
@@ -6,5 +7,8 @@ rescue
   odds.get_all_odds
   odds.save_all_odds
 end
-
-puts odds.to_h["response"][0]
+games_factory = GameFactory.new(odds.to_h["response"]).then{|gf| gf.create_games}
+games_factory.count.times do |i|
+  puts games_factory[i].game_name
+  puts games_factory[i].winning_each_way_bets
+end
