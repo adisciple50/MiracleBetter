a0259916b687e1917a9355d4661f4b541eb3ffaa
Jason Crockett <JesusisLord33333@outlook.com>
2023-03-13 18:03:51 +0000

    now getting fixtures

---
+++ fixtures.rb
@@ -0,0 +1,24 @@
+require 'uri'
+require 'net/http'
+require 'openssl'
+require_relative 'today'
+class Fixtures
+  include Today
+  def initialize
+    url = URI("https://api-football-v1.p.rapidapi.com/v3/fixtures?date=#{todays_date}")
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(url)
+    request["X-RapidAPI-Key"] = ENV["API_FOOTBALL_RAPID_API"]
+    request["X-RapidAPI-Host"] = 'api-football-v1.p.rapidapi.com'
+
+    response = http.request(request)
+    @fixtures = JSON::parse response.read_body.to_s
+  end
+  def get_fixture(fixture_id)
+    @fixtures["response"].select{|fixture| fixture["fixture"]["id"] == fixture_id}
+  end
+end
